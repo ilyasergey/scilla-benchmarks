cb47d3e37c6ab902c3a38447780a73acc382b332
kenchangh <guanhao3797@gmail.com>
2019-01-16 14:56:51 +0800

    removed burn function and working benchmark

--- benchmark/contracts/token.sol
+++ benchmark/contracts/token.sol
@@ -22,9 +22,6 @@ contract TokenERC20 {
     // This generates a public event on the blockchain that will notify clients
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
-
     /**
      * Constructor function
      *
@@ -123,38 +120,5 @@ contract TokenERC20 {
             spender.receiveApproval(msg.sender, _value, address(this), _extraData);
             return true;
         }
-    }
-
-    /**
-     * Destroy tokens
-     *
-     * Remove `_value` tokens from the system irreversibly
-     *
-     * @param _value the amount of money to burn
-     */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        totalSupply -= _value;                      // Updates totalSupply
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    /**
-     * Destroy tokens from other account
-     *
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-     *
-     * @param _from the address of the sender
-     * @param _value the amount of money to burn
-     */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        totalSupply -= _value;                              // Update totalSupply
-        emit Burn(_from, _value);
-        return true;
     }
 }
--- benchmark/evm_benchmark.py
+++ benchmark/evm_benchmark.py
@@ -24,11 +24,15 @@ contracts_dir = os.path.join(current_dir, 'contracts')
 SENDER_ADDRESS = '0xfaB8FcF1b5fF9547821B4506Fa0C35c68a555F90'
 SENDER_PRIVKEY = '4bc95d997c4c700bb4769678fa8452c2f67c9348e33f6f32b824253ae29a5316'
 
+devnull_file = open(os.devnull, 'w')
+
 
 def solc_compile_contract(contract_path, contract_name):
     output_path = os.path.join(output_dir, contract_name+'.bin')
-    subprocess.call(['solc', '--bin-runtime', '--optimize', '--overwrite',
-                     '-o', output_dir, contract_path])
+    subprocess.check_output(['solc', '--bin', '--optimize', '--overwrite',
+                             '-o', output_dir, contract_path],
+                            stderr=devnull_file)
+    print('Compiled {} to {}'.format(contract_name, output_path))
     bytecode = None
     with open(output_path) as f:
         bytecode = f.read()
@@ -50,7 +54,7 @@ def deploy_contract(bytecode, *constructor_args):
 
     call_args = [evm_exec, '--code', bytecode, '--datadir',
                  evm_data_dir, '--from', SENDER_ADDRESS]
-    deploy_output = subprocess.check_output(call_args)
+    deploy_output = subprocess.check_output(call_args, stderr=devnull_file)
 
     prefix = 'Contract Address: '
     prefix_pos = deploy_output.decode('utf-8').find(prefix)
@@ -68,9 +72,10 @@ def encode_input(function, *args):
 
 def perform_transaction(address, function, *args):
     encoded_input = encode_input(function, *args)
-    subprocess.call(
+    subprocess.check_output(
         [evm_exec, '--datadir', evm_data_dir, '--to', address,
-         '--input', encoded_input, '--from', SENDER_ADDRESS])
+         '--input', encoded_input, '--from', SENDER_ADDRESS],
+        stderr=devnull_file)
 
 
 def generate_random_params(arg_types):
@@ -92,13 +97,11 @@ def measure_evm_data_size():
 
 
 def measure_gas_cost(bytecode):
-    f = open(os.devnull, 'w')
-    debug_output = subprocess.check_output(
-        [evm_exec, '--debug', '--code', bytecode], stderr=f)
-    print(debug_output)
+    p = subprocess.Popen([evm_exec, '--debug', '--code', bytecode],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    run_output, debug_output = p.communicate()
     costs = re.compile('COST: (\d*)').findall(debug_output.decode('utf-8'))
     costs = [int(s) for s in costs]
-    print(costs)
     return sum(costs)
 
 
@@ -106,23 +109,38 @@ def run_tests(address, tests):
     for test_plan in tests:
         iterations = test_plan['iterations']
         function = test_plan['function']
-        print('Running {} iterations of {} function'.format(
+        print('Running {} iterations of `{}` function'.format(
             iterations, function.name))
 
         execution_times = []
 
         for iteration in range(iterations):
+            if iteration % 10 == 0:
+                print('Ran {} iterations'.format(iteration))
             args = generate_random_params(function.arg_types)
             start = time.time()
-            perform_transaction(address, function, args)
+            perform_transaction(address, function, *args)
             end = time.time()
             execution_times.append(end-start)
 
         print('Ran {} iterations of {} function'.format(
             iterations, function.name))
-        print('New database size: {}'.format(measure_evm_data_size()))
-        print('Median execution time: {}'.format(median(execution_times)))
-        print('Mean execution time: {}'.format(mean(execution_times)))
+        print('New database size: {:,} bytes'.format(measure_evm_data_size()))
+        print('Median execution time: {0:.6f} seconds'.format(
+            median(execution_times)))
+        print('Mean execution time: {0:.6f} seconds'.format(
+            mean(execution_times)))
+        print()
+
+
+def populate_evm_state(address, transactions):
+    print(
+        '\nPopulating EVM state with {} transactions...'.format(
+            len(transactions)))
+    for index, tx_plan in enumerate(transactions):
+        if index % 10 == 0:
+            print('Executed {} transactions'.format(index))
+        perform_transaction(address, *tx_plan)
 
 
 def run_benchmark(contract_plan):
@@ -132,15 +150,16 @@ def run_benchmark(contract_plan):
         contract_path, contract_plan['contract_name'])
     address = deploy_contract(bytecode, *contract_plan['constructor'])
     print('Contract deployed at:', address)
-    print('Contract bytecode size:', len(bytecode.encode('utf-8')))
+    print('Contract bytecode size: {:,} bytes'.format(
+        len(bytecode.encode('utf-8'))))
 
-    # populating the contract state
-    for tx_plan in contract_plan['transactions']:
-        perform_transaction(address, *tx_plan)
+    populate_evm_state(address, contract_plan['transactions'])
 
-    print('Initial EVM database size:', measure_evm_data_size())
+    print('\nInitial EVM database size: {:,} bytes\n'.format(
+        measure_evm_data_size()))
     run_tests(address, contract_plan['tests'])
-    print('Final EVM database size:', measure_evm_data_size())
+    print('Final EVM database size: {:,} bytes'.format(
+        measure_evm_data_size()))
 
 
 class ContractFunction():
@@ -171,29 +190,25 @@ def main():
             'constructor': (
                 ('uint256', 'string', 'string'),
                 (total_token_supply, 'Test', 'TEST')),
-            #'transactions': [
-            #    (
-            #        ContractFunction('transfer', ('address', 'uint256')),
-            #        addr, 1*(10**16)
-            #    )
-            #    for addr in get_addresses()
-            #],
-            #'tests': [
-            #    {
-            #        'function': ContractFunction(
-            #            'transfer', ('address', 'uint256')),
-            #        'iterations': 100
-            #    },
-            #    {
-            #        'function': ContractFunction('burn', ('uint256')),
-            #        'iterations': 100
-            #    },
-            #    {
-            #        'function': ContractFunction(
-            #            'approve', ('address', 'uint256')),
-            #        'iterations': 100
-            #    },
-            #]
+            'transactions': [
+                (
+                    ContractFunction('transfer', ('address', 'uint256')),
+                    addr, 1*(10**16)
+                )
+                for addr in get_addresses()[:10]
+            ],
+            'tests': [
+                {
+                    'function': ContractFunction(
+                        'transfer', ('address', 'uint256')),
+                    'iterations': 100
+                },
+                {
+                    'function': ContractFunction(
+                        'approve', ('address', 'uint256')),
+                    'iterations': 100
+                },
+            ]
         }
 
     ]
