2664f4e7c632b6f5532c3670c33c3b9bc746c0c3
kenchangh <guanhao3797@gmail.com>
2019-01-25 16:44:50 +0800

    working tests for auction & crowdfunding

--- scilla-benchmark/scilla_benchmark.py
+++ scilla-benchmark/scilla_benchmark.py
@@ -3,20 +3,23 @@ import re
 import sys
 import time
 import json
+import uuid
 import subprocess
 from statistics import median, mean
 from benchmark_plans import get_benchmark_plans
 
 
-scilla_runner_exec = '/home/scilla/bin/scilla-runner'
+scilla_dir = '/home/scilla'
+scilla_runner_exec = os.path.join(scilla_dir, 'bin', 'scilla-runner')
+std_lib = os.path.join(scilla_dir, 'src', 'stdlib')
 current_dir = os.path.dirname(os.path.realpath(__file__))
 contracts_dir = os.path.join(current_dir, 'contracts')
 blockchain_json = os.path.join(contracts_dir, 'blockchain.json')
 output_dir = os.path.join(current_dir, 'output')
-message_json = os.path.join(contracts_dir, 'message.json')
 
 
-def create_message_file(tag, amount, sender, params):
+def create_message_file(contract_name, tag, amount, sender, params):
+    message_json = os.path.join(contracts_dir, contract_name, 'message.json')
     message = {
         "_tag": tag,
         "_amount": amount,
@@ -34,23 +37,24 @@ def create_state_file(contract_name, state):
     return state_path
 
 
-def run_test(contract_name, transaction):
+def run_test(contract_name, transaction, blockchain_json=blockchain_json):
     test_dir = os.path.join(contracts_dir, contract_name)
     contract_path = os.path.join(test_dir, 'contract.scilla')
     init_json = os.path.join(test_dir, 'init.json')
     state_json = os.path.join(test_dir, 'state.json')
+    message_json = os.path.join(test_dir, 'message.json')
     output_filepath = os.path.join(output_dir, contract_name)
 
     tag = transaction['transition']
     params = transaction['params']
     amount = transaction['amount']
     sender = transaction['sender']
-    create_message_file(tag, amount, sender, params)
+    create_message_file(contract_name, tag, amount, sender, params)
 
     command = [scilla_runner_exec, '-init', init_json, '-istate', state_json,
                '-iblockchain', blockchain_json, '-imessage', message_json,
-               '-o', output_filepath, '-i', contract_path, '-libdir', 'src/stdlib',
-               '-gaslimit', '10000']
+               '-o', output_filepath, '-i', contract_path, '-libdir', std_lib,
+               '-gaslimit', '10000000']
     output = subprocess.check_output(command)
     match = re.search(b'time:(.*)', output)
     time_taken = float(match[1]) * 1000
@@ -69,9 +73,15 @@ def run_benchmark(no_of_state_entries, iterations=100):
             execution_times = []
             test_name = test_plan['test_name']
             transactions = test_plan['transactions']
+            blockchain_filename = test_plan.get('blockchain')
             iterations = len(transactions)
             print('Running {} iterations of `{}` function'.format(
                 iterations, test_name))
+
+            bjson = blockchain_json
+            if blockchain_filename:
+                bjson = os.path.join(
+                    contracts_dir, blockchain_filename)
 
             if iterations == 0:
                 raise Exception(
@@ -80,9 +90,11 @@ def run_benchmark(no_of_state_entries, iterations=100):
             for iteration, transaction in enumerate(transactions):
                 if iteration % 10 == 0:
                     print('Ran {} iterations'.format(iteration))
-                time_taken = run_test(contract_name, transaction)
+                time_taken = run_test(
+                    contract_name, transaction, blockchain_json=bjson)
                 execution_times.append(time_taken)
 
+            print('Using {:,} state entries...'.format(no_of_state_entries))
             print('Ran {} iterations of `{}` function'.format(
                 iterations, test_name))
             # print('New database size: {:,} bytes'.format(
