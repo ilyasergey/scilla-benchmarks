8a2d20aeb385e4b68947cdf79b49315dd0a9ce33
kenchangh <guanhao3797@gmail.com>
2019-07-06 15:29:07 +0800

    printing data table out

--- breakdown.py
+++ breakdown.py
@@ -44,6 +44,12 @@ def run_breakdown():
         size = STATE_SIZES[index]
         state_breakdown[size] = result
 
+    print('\nRESULTS:')
+    table_data = transform_to_table_data(state_breakdown)
+    print_table_data(table_data)
+    print()
+
+    print('Showing the bar chart...')
     plot_data = transform_to_plot_data(state_breakdown)
     plot_comparison_bar_chart(plot_data)
 
@@ -91,7 +97,67 @@ def transform_to_plot_data(data):
     return plot_data
 
 
+def transform_to_table_data(data):
+    table_data = []
+
+    for function_name in FUNCTION_NAMES:
+        time_data = []
+
+        for time in TIME_NAMES:
+            size_data = []
+
+            for size in STATE_SIZES:
+                size_data.append(data[size][function_name][time])
+            time_data.append(size_data)
+        table_data.append(time_data)
+
+    return table_data
+
+
+def print_table_data(table_data):
+    left_margin = 22
+    data_left_padding = 10
+    group_separator = ' | '
+
+    upper_header_row = ' ' * left_margin + group_separator
+
+    for time_name in TIME_NAMES:
+        upper_header_row += ('{:^'+str(data_left_padding*3) +
+                             's}').format(time_name)
+        upper_header_row += group_separator
+    print(upper_header_row)
+
+    upper_header_div = ' ' * left_margin + group_separator
+
+    for _ in range(len(TIME_NAMES)):
+        upper_header_div += '-' * (data_left_padding * 3)
+        upper_header_div += ' + '
+    print(upper_header_div)
+
+    lower_header_row = (
+        '{:<'+str(left_margin)+'}').format('Transition/State size')
+    lower_header_row += group_separator
+
+    for _ in range(len(TIME_NAMES)):
+        for size in STATE_SIZES:
+            size_header = str(size//1000)+'k'
+            lower_header_row += ('{:>'+str(data_left_padding) +
+                                 '}').format(size_header)
+        lower_header_row += group_separator
+    print(lower_header_row)
+
+    for function_index, function_data in enumerate(table_data):
+        function_name = FUNCTION_NAMES[function_index]
+        row_str = ('{:<'+str(left_margin)+'}').format(function_name)
+        row_str += group_separator
+
+        for time_data in function_data:
+            for time in time_data:
+                row_str += ('{:>'+str(data_left_padding) +
+                            '}').format(round(time, 2))
+            row_str += group_separator
+        print(row_str)
+
+
 if __name__ == '__main__':
-    # plot_data = transform_to_plot_data()
-    # plot_comparison_bar_chart(plot_data)
     run_breakdown()
